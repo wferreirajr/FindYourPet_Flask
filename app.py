4fe8f15a68a79f028643638b7f066b8f7b5dd16b
Wilson Ferreira Jr <wilson.f.jr@gmail.com>
2022-05-14 23:20:46 -0300

    .

--- app.py
+++ app.py
@@ -21,6 +21,10 @@ def login():
 def registro():
     return render_template('registro.html')
 
+@app.route('/sair')
+def sair():
+    return '<h1>TEM QUE SAIR DO SISTEMA</h1>'
+
 @app.route('/admin')
 def dashboard():
     return render_template('dashboard.html', mactive_dash='active')
@@ -28,6 +32,10 @@ def dashboard():
 @app.route('/admin/adduser')
 def adduser():
     return render_template('adduser.html', mactive_admin='active', mexpand='true')
+
+@app.route('/admin/addanimal')
+def addanimal():
+    return render_template('reg_aminalzinho.html', mactive_animal='active')
 
 @app.route('/createusr/', methods=('GET', 'POST'))
 def create_user():
@@ -86,6 +94,37 @@ def create_user_admin():
 
     return render_template('registro.html')
 
+@app.route('/registeranimal/', methods=('GET', 'POST'))
+def register_aminal():
+    if request.method == 'POST':
+        nome = request.form['amnNomeCompleto']
+        foto = request.form['amnFoto']
+        especie = request.form['amnEspecie']
+        sexo = request.form['amnSexo']
+        idade = request.form['amnIdade']
+        porte = request.form['amnPorte']
+        sobre = request.form['amnSobre']
+
+        cep = request.form['edrCep']
+        endereco = request.form['edrRua']
+        numero = request.form['edrNumero']
+        complemento = request.form['edrComplemento']
+        bairro = request.form['edrBairro']
+        cidade = request.form['edrCidade']
+        estado = request.form['edrEstado']
+
+        if not nome:
+            flash('O nome é requedido!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO aminals (NomeCompleto, Foto, Especie, Sexo, Idade, Porte, Sobre, Cep, Rua, Numero, Complemento, Bairro, Cidade, Estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (nome, foto, especie, sexo, idade, porte, sobre, cep, endereco, numero, complemento, bairro, cidade, estado))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('addanimal'))
+
+    return render_template('registro.html')
+
+
 # app.run(host='0.0.0.0', port='8081', debug=True)
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0', debug=1)
--- db/schema.sql
+++ db/schema.sql
@@ -17,3 +17,22 @@ CREATE TABLE "accounts" (
         "Cidade" TEXT,
         "Estado" TEXT
 );
+
+CREATE TABLE "aminals" (
+        "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+        "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "NomeCompleto" TEXT NOT NULL,
+        "Foto" TEXT,
+        "Especie" TEXT,
+        "Sexo" TEXT,
+        "Idade" TEXT,
+        "Porte" TEXT,
+        "Sobre" TEXT,
+        "Cep" TEXT,
+        "Rua" TEXT,
+        "Numero" TEXT,
+        "Complemento" TEXT,
+        "Bairro" TEXT,
+        "Cidade" TEXT,
+        "Estado" TEXT
+);
